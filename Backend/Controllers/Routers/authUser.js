22d968f7468ed4e87cebe5dcfb0922d522bebbe6
Mazeghrane Oussama <loulououssama2015@gmail.com>
2022-05-26 13:08:38 +0100

    ...

--- Backend/Controllers/Routers/authUser.js
+++ Backend/Controllers/Routers/authUser.js
@@ -1,7 +1,24 @@
-const sign = (req, res) => {
-  res.status(200).send(req.query);
-};
-const login = async (req, res) => {
-  res.status(200).send("hello world");
-};
-module.exports = { sign, login };
+const { sign_validation_user } = require("../../Validations/UserValidation");
+
+async function sign(req, res) {
+  try {
+    let { error } = await sign_validation_user(req.body);
+    if (error)
+      res.status(404).json({
+        status: "failed",
+        err: error.details[0],
+      });
+    res.status(200).json({
+      status: "success",
+      message: `welcome ${req.body.firstname}`,
+    });
+  } catch (err) {
+    res.status(401).send(err.message);
+  }
+}
+
+function login(req, res) {
+  console.log(req.body);
+  res.status(201).send("hey");
+}
+module.exports = { login, sign };
