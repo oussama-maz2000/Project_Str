caee00ca204eae0b26d85d2d2797088fd08e7a11
oussamaMAZ <70782166+oussama-maz2000@users.noreply.github.com>
2022-06-19 17:15:38 +0100

    ...

--- Backend/app.js
+++ Backend/app.js
@@ -3,20 +3,21 @@ const connection = require("./helps/connection");
 const router = require("./Generate/UserGenerate");
 const bodyparser = require("body-parser");
 const { GlobalErr } = require("./helps/Error");
+const {graphqlHTTP}=require('express-graphql')
+const resolvers=require("./GraphQl/Root");
+const schema=require("./GraphQl/Schema");
 const app = express();
 connection;
-app.use(bodyparser.text());
-app.use(express.json());
-app.use("/user", router);
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: resolvers,
+    graphiql: true,
+  })
+);
 
-app.get("/", (req, res) => {
-  res.status(200).send("welcome in BackEnd store 😎");
-});
-
-app.post("/setname", (req, res) => {
-  console.log(req.body);
-});
 app.listen(3001, (req, res) => {
   console.log("server run on 3001 😎");
 });
-app.use(GlobalErr);
+//app.use(GlobalErr);
